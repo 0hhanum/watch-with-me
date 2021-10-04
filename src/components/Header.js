836443b452d4043b37978360e98589e966c7c844
hanum <rntls123@naver.com>
2021-10-05 00:58:25 +0900

    10.5 <Link> instead <a>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
-
 // styled-components 를 이용해 react component 를 생성.
 // **style 이 포함된**component 를 만들 수 있게 된다.
+import { Link } from "react-router-dom";
+// 앱 내에 link 가 가려는 페이지가 있으면, 브라우저의 방식이 아닌 js 의 방식으로 이동하게 해준다.
+// 단 <Router> 내에서 사용해야 한다.
+
+const Header = styled.header``;
 
 const List = styled.ul` // 백틱
     display: flex;
@@ -10,19 +14,24 @@ const List = styled.ul` // 백틱
     }
 `;
 
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
+
 export default () => (
-    <header>
+    <Header>
         {/* styled component <List> 를 이용. */}
         <List>
-            <li>
-                <a href="/">영화</a>
-            </li>
-            <li>
-                <a href="/tv">TV</a>
-            </li>
-            <li>
-                <a href="/search">찾아보기</a>
-            </li>
+            <Item>
+                <SLink to="/">영화</SLink>
+                {/* react-router <Link> 는 to 를 사용. */}
+            </Item>
+            <Item>
+                <SLink to="/tv">TV</SLink>
+            </Item>
+            <Item>
+                <SLink to="/search">찾아보기</SLink>
+            </Item>
         </List>
-    </header>
+    </Header>
 )
--- src/components/Router.js
+++ src/components/Router.js
@@ -4,17 +4,22 @@ import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 
+import Header from "Components/Header";
+
 export default () => {
     // switch => 한번에 하나의 Route 만 render 하도록 해서 redirect 와 겹치지 않게해줌.
     return (
         <Router>
-            <Switch>
-                <Route path="/" exact component={Home}></Route>
-                <Route path="/tv" exact component={TV}></Route>
-                <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
-                <Route path="/search" exact component={Search}></Route>
-                <Redirect from="*" to="/" />
-            </Switch>
+            <>
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/tv" exact component={TV}></Route>
+                    <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+                    <Route path="/search" exact component={Search}></Route>
+                    <Redirect from="*" to="/" />
+                </Switch>
+            </>
         </Router>
     )
 }
