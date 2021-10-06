aa091eccf9db8f457c409463e9774b4b2aa289d2
hanum <rntls123@naver.com>
2021-10-06 22:51:54 +0900

    10.6 Home container (So sexy)

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,4 +1,5 @@
 // Container 는 render 외의 로직, state, 데이터, api 호출 등을 담당.
+import { moviesApi } from "api";
 import React from "react";
 import HomePresenter from "./HomePresenter";
 
@@ -9,6 +10,25 @@ export default class extends React.Component {
         upcoming: null,
         error: null,
         loading: true,
+    }
+
+    async componentDidMount() {
+        try {
+            const { data: { results: nowPlaying } } = await moviesApi.nowPlaying();
+            const { data: { results: upComing } } = await moviesApi.upcoming();
+            const { data: { results: popular } } = await moviesApi.popular();
+            this.setState({
+                nowPlaying, upComing, popular
+            })
+        } catch {
+            this.setState({
+                error: "문제가 발생했습니다. 영화를 가져올 수 없습니다."
+            })
+        } finally {
+            this.setState({
+                loading: false
+            });
+        }
     }
 
     render() {
